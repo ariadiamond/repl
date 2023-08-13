a3b9b2b664fbbfc788fea2d51eaeae6a8203d126
Aria Diamond <59593260+ariadiamond@users.noreply.github.com>
2023-08-13 08:23:07 -0400

    add lodash

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import _ from 'lodash';
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+window._ = _;
+window.lodash = _;
 root.render(
   <React.StrictMode>
     <App />
